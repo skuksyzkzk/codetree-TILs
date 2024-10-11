cfd0358fbb5b7d9cd9fd8c3632b25f331d534970
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-10-11 16:33:42 +0900

    Title: 색깔 트리, Time: 430ms, Memory: 4MB, Status: Passed - CodeTree

--- 241011/색깔 트리/color-tree.cpp
+++ 241011/색깔 트리/color-tree.cpp
@@ -14,14 +14,16 @@ struct Node {
 int q, type;
 Node nodes[100006];
 vector<int> roots;
+const int COLOR_MAX = 5;
 
-// 깊이체크
+// 깊이체크 함수
 bool check_depth(Node p, int depth) {
     if (p.id == 0) return true;
     if (p.max_depth <= depth) return false;
     return check_depth(nodes[p.p], depth + 1);
 }
-// 삽입
+
+// 삽입 함수
 void insert() {
     int m, p, c, d;
     cin >> m >> p >> c >> d;
@@ -44,9 +46,9 @@ void insert() {
     if (p != -1) {
         nodes[p].child.push_back(m);
     }
-    
 }
-// 해당 하는 m_id의 서브트리를 전부 바꿔야함 
+
+// 색 변경 함수
 void change_color() {
     int m, newcolor;
     cin >> m >> newcolor;
@@ -59,37 +61,60 @@ void change_color() {
             s.push(nxt);
         }
     }
-    
 }
+
+// 색 조회 함수
 void search_color() {
     int m;
     cin >> m;
     cout << nodes[m].color << "\n";
 }
-void value_sum() {
-    long long totalBeauty = 0;
-    for (int rootId : roots) {
-        stack<int> s;
-        s.push(rootId);
-        unordered_set<int> distinctColors;
 
-        while (!s.empty()) {
-            int curId = s.top(); s.pop();
-            Node& cur = nodes[curId];
-            distinctColors.insert(cur.color);
+// 서브트리의 색깔 정보를 기반으로 점수 계산
+pair<int, vector<int>> calculate_subtree_beauty(int curId) {
+    Node& cur = nodes[curId];
+    // 색깔을 카운팅하기 위한 배열, 색깔의 범위는 1부터 COLOR_MAX까지
+    vector<int> colorCount(COLOR_MAX + 1, 0);
+    colorCount[cur.color] = 1; // 현재 노드의 색깔을 기록
 
-            for (int childId : cur.child) {
-                s.push(childId);
-            }
+    int subtreeScore = 0;
+    
+    // 자식 노드들을 순회하면서 서브트리 점수와 색깔 정보를 가져옴
+    for (int childId : cur.child) {
+        auto [childScore, childColorCount] = calculate_subtree_beauty(childId);
+        
+        // 자식 노드에서 가져온 색깔 정보를 부모 노드에 합산
+        for (int i = 1; i <= COLOR_MAX; i++) {
+            colorCount[i] += childColorCount[i];
         }
 
-        totalBeauty += distinctColors.size() * distinctColors.size();
+        // 자식 서브트리에서의 점수를 부모 노드에 합산
+        subtreeScore += childScore;
     }
-    cout << totalBeauty << "\n";
+
+    // 서로 다른 색깔의 개수를 계산하여 점수에 반영
+    int distinctColors = 0;
+    for (int i = 1; i <= COLOR_MAX; i++) {
+        if (colorCount[i] > 0) distinctColors++;
+    }
+    subtreeScore += distinctColors * distinctColors;
+
+    return make_pair(subtreeScore, colorCount);
+}
+
+// 전체 트리의 점수를 계산하는 함수
+void value_sum() {
+    long long total_value = 0;
+
+    // 각 루트 노드에 대해 서브트리의 beauty 값을 계산
+    for (int rootId : roots) {
+        total_value += calculate_subtree_beauty(rootId).first;
+    }
+
+    cout << total_value << "\n";
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> q;
     nodes[0].id = 0; // 루트의 루트는 0
